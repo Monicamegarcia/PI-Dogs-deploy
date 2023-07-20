68e6533b76d4fca3c90c4ef543f1ee3b230b0dfa
Monicamegarcia <monicamegarcia@gmail.com>
2023-07-20 17:59:18 -0300

    port

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require("dotenv").config();
-const port = process.env.PORT || 3000;
+const port = 3000;
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   console.log("Database connected");
