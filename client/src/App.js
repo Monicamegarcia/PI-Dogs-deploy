94e3f92a68ccc02f84a8d51cbbb70377e7181f6e
Monicamegarcia <monicamegarcia@gmail.com>
2023-07-20 16:35:37 -0300

    new back

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { Landing, Home, Detail, Form, About, Exiting, Error } from "./views"
 import { Navbar } from './components';
 import { Route, Routes, useLocation } from "react-router-dom";
 import axios from 'axios';
-axios.defaults.baseURL = "http://localhost:3001"
+axios.defaults.baseURL = "https://pi-dogs-deploy-production-c134.up.railway.app/"
 
 function App() {
 
